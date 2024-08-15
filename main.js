3af442ae6cdde30d1130a7d9624acc9a14f01069
Kaikear <129184714+Kaikear@users.noreply.github.com>
2024-08-15 09:24:15 -0300

    adsa

--- main.js
+++ main.js
@@ -1,5 +1,1 @@
-const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResultado = document.querySelector(".texto-resultado");
+const test = document.querySelector(".caixa-principal");

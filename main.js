6eedc9be3a2e4edba9fa08b8b87a9c739898733d
Kaikear <129184714+Kaikear@users.noreply.github.com>
2024-08-20 08:38:51 -0300

    xzx

--- main.js
+++ main.js
@@ -2,5 +2,14 @@ const elemento = document.querySelector(".teste");
 
 const titulo = document.querySelector(".teste2");
 
-
 titulo.textContent = "ola mundo"
+
+
+let contador = 1 ;
+
+let contador = [1,2,5,8,9];
+
+let perguntas = {
+
+    []
+}
